ecb073a778a1d56759174379a619540a910e98f6
Jithin <jithink6311@gmail.com>
2022-02-15 21:51:41 +0530

    second commit

---
+++ backend/data.js
@@ -0,0 +1,77 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Nike Slim Shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:120,
+            countInStock:10,
+            brand:"nike",
+            rating:4.6,
+            numReviews:125,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            name:'Adidas fit Shirt',
+            category:'shirts',
+            image:'/images/p2.jpg',
+            price:100,
+            countInStock:20,
+            brand:"Adidas",
+            rating:3,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'3',
+            name:'Lacoste tee Shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:170,
+            countInStock:0,
+            brand:"Lacoste",
+            rating:4.5,
+            numReviews:105,
+            description:'high quality product'
+        },
+        {
+            _id:'4',
+            name:'puma Slim Shirt',
+            category:'shirts',
+            image:'/images/p2.jpg',
+            price:160,
+            countInStock:30,
+            brand:"nike",
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'5',
+            name:'Nike Slim Shirt',
+            category:'shirts',
+            image:'/images/p1.jpg',
+            price:320,
+            countInStock:0,
+            brand:"nike",
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'6',
+            name:'reebok Slim Shirt',
+            category:'shirts',
+            image:'/images/p2.jpg',
+            price:170,
+            countInStock:14,
+            brand:"reebok",
+            rating:4,
+            numReviews:10,
+            description:'high quality product'
+        }
+    ]
+}
+export default data;
